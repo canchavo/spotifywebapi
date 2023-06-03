86cb597dc7d64930e4895439b147190f7e03b0a3
canchavo <103379410+canchavo@users.noreply.github.com>
2023-06-03 13:06:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var redirect_uri = "https://makeratplay.github.io/SpotifyWebAPI/"; // change this your value
+var redirect_uri = "https://spotifywebapi-seven.vercel.app/"; // change this your value
 //var redirect_uri = "http://127.0.0.1:5500/index.html";
  
 
